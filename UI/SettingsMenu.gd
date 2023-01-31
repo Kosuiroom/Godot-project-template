db211f03b695cd36d89921bee0d3d478fd48926d
Pontus <pontus.goth@outlook.com>
2023-01-31 19:12:20 +0100

    added possibility to remap input

--- UI/SettingsMenu.gd
+++ UI/SettingsMenu.gd
@@ -3,10 +3,6 @@ extends Control
 onready var audio = $Test/AudioStreamPlayer
 onready var action_list = $SettingTabs/Input/MarginContainer/VBoxContainer/ScrollContainer/ActionList
 
-
-
-
-
 func _on_MasterVolumeSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), value)
 
